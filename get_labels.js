7f4bcb845b18bc63f5241040be32ea6bc3d27ec0
Ziad Mallah <ziad@elmallah.com>
2017-02-04 17:06:52 -0500

    created getLabels function

---
+++ get_labels.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**/
+
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+
+
+// Create the Rekognition client
+var rekognition = new AWS.Rekognition();
+
+function get_labels(image){
+    params = getBase64Image(img);
+    rekognition.compareFaces(params, function(err, data)) {
+        if (err)  console.log(err, err.stack);
+        else      console.log(data);
+    });
+}
+
+function getBase64Image(img) {
+    // Create an empty canvas element
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    // Copy the image contents to the canvas
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    // Get the data-URL formatted image
+    // Firefox supports PNG and JPEG. You could check img.src to
+    // guess the original format, but be aware the using "image/jpg"
+    // will re-encode the image.
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
