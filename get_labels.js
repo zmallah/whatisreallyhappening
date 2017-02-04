b582f17150b6dc6cd8b2180d64243ad823acdf3c
Ziad Mallah <ziad@elmallah.com>
2017-02-04 17:21:14 -0500

    made slight changes

--- get_labels.js
+++ get_labels.js
@@ -36,14 +36,10 @@ function getBase64Image(img) {
     canvas.width = img.width;
     canvas.height = img.height;
 
-    // Copy the image contents to the canvas
     var ctx = canvas.getContext("2d");
+    // Copy the image contents to the canvas
     ctx.drawImage(img, 0, 0);
 
-    // Get the data-URL formatted image
-    // Firefox supports PNG and JPEG. You could check img.src to
-    // guess the original format, but be aware the using "image/jpg"
-    // will re-encode the image.
     var dataURL = canvas.toDataURL("image/png");
 
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
